570ed3e96df2185b63cc1739d68df6f08283b711
Abdur Rehman <malik.a.rehman04@gmail.com>
2025-08-05 16:40:20 +0500

    feat: Added Clerk Auth

--- app/auth/register/action.ts
+++
@@ -1,7 +0,0 @@
-'use server'
-export async function SubmitData(formData: FormData){
-    const name= formData.get('name');
-    const email= formData.get('email');
-
-    console.log("Registered Successfully: ", name,email);
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center mx-auto">
       <p >Welcome to Home Services Platform</p>
-      <div className="flex justify-center mx-auto">
-        <Link href={"/auth/login"}>Login</Link>
-        <Link href={"/auth/register"}>Register</Link>
-      </div>
     </div>
   );
 }
